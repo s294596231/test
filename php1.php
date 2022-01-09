d539700cad8f53433f91581cb51df1c600c789a5
s294596231 <57859998+s294596231@users.noreply.github.com>
2022-01-09 23:11:35 +0800

    Create php1.php

---
+++ php1.php
@@ -0,0 +1,24 @@
+<?php 
+    function king ($n,$m)
+    {
+        $monkey = range(1, $n);
+        $i = 0;
+        while (count($monkey)>1) {
+            $i++;
+            $head = array_shift($monkey);
+            if ($i % $m !=0) {
+                array_push($monkey, $head);# code...
+            }
+        }
+        return ['total'=>$n,'kick' =>$m,'king' =>$monkey[0]];
+    }
+
+        
+
+
+
+
+
+        $data = king(10,7);
+        print_r(king($data));
+ ?>
